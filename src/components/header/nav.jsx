73e3098afac5c83aa05a176179dda5b0df1a7a0c
Izumi <aiza.izumi@gmail.com>
2020-12-15 14:49:51 +0600

    added some changes

---
+++ src/components/header/nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+    const navItems = ['О платформе',  ]
+    return (
+        <nav className="header__nav">
+            <ul>
+                {navItems}
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
