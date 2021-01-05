5dcc486a571faca469bd7f31db68fc46979afc84
Izumi <aiza.izumi@gmail.com>
2021-01-05 20:06:41 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import './style.scss';
+import Header from './components/header';
+import Filter from './components/filter';
+import Table from './components/table';
 
 const App = () => {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Header />
+      <Filter />
+      <Table />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { navItems } from './const';
 
-const Nav = () => {
-    const navItems = ['О платформе',  ]
-    return (
-        <nav className="header__nav">
-            <ul>
-                {navItems}
-            </ul>
-        </nav>
-    )
-}
+const Nav = ({ className }) => {
+  const [activeClass, setActiveClass] = React.useState(0);
 
-export default Nav
+  const handleClick = (index) => {
+		setActiveClass(index)
+	};
+
+  return (
+    <nav className={`nav ${className}`}>
+      <ul className='nav__list'>
+        {navItems.map((item, index) => {
+          return (
+            <li
+              onClick={() => handleClick(index)}
+              key={item}
+              className={`nav__item ${activeClass === index ? 'is-active' : ''}`}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
